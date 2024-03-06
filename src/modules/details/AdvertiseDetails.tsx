0b7ef2278d2ca270701a10fd6e109a188c0e4bad
Alexey <kaluzhniy2017@yandex.ru>
2024-03-06 18:00:38 +0200

    style details screen

--- src/modules/details/AdvertiseDetails.tsx
+++ src/modules/details/AdvertiseDetails.tsx
@@ -1,12 +1,62 @@
 import {NavigationFunctionComponent} from "react-native-navigation";
-import {Text, View} from "react-native";
+import {Image, ImageURISource, StyleSheet, View} from "react-native";
 import {CustomHeader} from "~/components/CustomHeader";
+import React from "react";
+import {CommonSizes} from "~/core/theme/commonSizes";
+import {Roboto} from "~/infrastructure";
+import {CommonStyles} from "~/core/theme/commonStyles";
+import {LightThemeColors} from "~/core/theme/colors";
 
-export const AdvertiseDetails: NavigationFunctionComponent = (props) => {
+interface IProps {
+    item: {
+        id: string;
+        title: string;
+        body: string;
+        price: string;
+        location: string;
+        date: string;
+        image: ImageURISource | null;
+    };
+}
+
+export const AdvertiseDetails: NavigationFunctionComponent<IProps> = (props) => {
     return (
         <View>
             <CustomHeader id={props.componentId} isStack isDetails/>
-            <Text>Детали</Text>
+            <View style={styles.contentContainer}>
+                {props.item.image &&
+                    <Image source={props.item.image} style={styles.image}/>}
+                <View style={[CommonStyles.rowCenter, styles.locationContainer]}>
+                    <Roboto.Body.Medium text={props.item.date}/>
+                    <Roboto.Body.Medium text={props.item.location}/>
+                </View>
+                <Roboto.Title.Large text={props.item.title} style={styles.text}/>
+                <Roboto.Title.Large text={props.item.price} style={styles.text}/>
+                <Roboto.Body.Small text={"Описание"} style={styles.text}/>
+                <Roboto.Body.Medium text={props.item.body} style={styles.text}/>
+            </View>
+            <View style={styles.outline}/>
         </View>
     );
 };
+
+const styles = StyleSheet.create({
+    contentContainer: {
+        marginHorizontal: CommonSizes.margin.largePlus
+    },
+    image: {
+        borderRadius: CommonSizes.borderRadius.extraLargePlus,
+        marginBottom: CommonSizes.margin.largePlus
+    },
+    locationContainer: {
+        justifyContent: 'space-between',
+    },
+    text: {
+        marginTop: CommonSizes.margin.small
+    },
+    outline: {
+        height: 1,
+        marginVertical: CommonSizes.margin.largePlus,
+        backgroundColor: LightThemeColors.outline
+    }
+});
--- src/modules/favorite/Favorite.tsx
+++ src/modules/favorite/Favorite.tsx
@@ -1,8 +1,6 @@
 import {Text, View} from "react-native";
 import {NavigationFunctionComponent} from "react-native-navigation";
 import React from "react";
-import {Components} from "~/navigation/components";
-import {i18next} from "~/common/localization/localization";
 import {CustomHeader} from "~/components/CustomHeader";
 import {AddButton} from "~/components/AddButton";
 import {CommonStyles} from "~/core/theme/commonStyles";
@@ -16,18 +14,3 @@ export const Favorite: NavigationFunctionComponent = (props): JSX.Element => {
         </View>
     );
 };
-
-Favorite.options = {
-    topBar: {
-        title: {
-            component: {
-                id: Components.topBarHeader.id,
-                name: Components.topBarHeader.name,
-                passProps: {
-                    title: i18next.t("pages.favorite"),
-                    isProfile: false,
-                }
-            },
-        }
-    }
-}
