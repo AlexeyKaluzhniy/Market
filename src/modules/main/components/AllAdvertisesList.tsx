dea45c3c23208df21688d71d2fb0f1601b9c1228
Alexey <kaluzhniy2017@yandex.ru>
2024-03-18 09:50:02 +0200

    move mock data to separate file

--- src/modules/main/components/AllAdvertisesList.tsx
+++ src/modules/main/components/AllAdvertisesList.tsx
@@ -1,49 +1,9 @@
 import {FlatList, StyleSheet} from "react-native";
 import {ListItem} from "~/modules/main/components/ListItem";
-import React, {useState} from "react";
 import {CommonSizes} from "~/core/theme/commonSizes";
-import {AdvertisesResources} from "~/common/ImageResources.g";
+import {data} from "~/infrastructure/mocks/mockAdvertises";
 
 export function AllAdvertisesList() {
-    const [data] = useState([
-        {
-            id: '1',
-            title: 'Продажа велосипеда',
-            body: 'Почти новый горный велосипед, бренд XYZ, 2023 года выпуска. Использовался всего несколько месяцев',
-            price: '1200 руб.',
-            location: 'Тирасполь',
-            date: 'сегодня в 13:04',
-            image: AdvertisesResources.bicycle
-        },
-        {
-            id: '2',
-            title: 'Продажа велосипеда',
-            body: 'Почти новый горный велосипед, бренд XYZ, 2023 года выпуска. Использовался всего несколько месяцев',
-            price: '1200 руб.',
-            location: 'Тирасполь',
-            date: 'сегодня в 13:04',
-            image: null
-        },
-        {
-            id: '3',
-            title: 'Продажа велосипеда',
-            body: 'Почти новый горный велосипед, бренд XYZ, 2023 года выпуска. Использовался всего несколько месяцев',
-            price: '1200 руб.',
-            location: 'Тирасполь',
-            date: 'сегодня в 13:04',
-            image: null
-        },
-        {
-            id: '4',
-            title: 'Продажа велосипеда',
-            body: 'Почти новый горный велосипед, бренд XYZ, 2023 года выпуска. Использовался всего несколько месяцев',
-            price: '1200 руб.',
-            location: 'Тирасполь',
-            date: 'сегодня в 13:04',
-            image: AdvertisesResources.bicycle
-        }
-    ]);
-
     return (
         <FlatList
             data={data}
--- src/modules/main/components/MyAdvertisesList.tsx
+++ src/modules/main/components/MyAdvertisesList.tsx
@@ -1,38 +1,26 @@
-import {ListItem} from "~/modules/main/components/ListItem";
 import {FlatList, StyleSheet} from "react-native";
-import React, {useState} from "react";
-import {CommonSizes} from "~/core/theme/commonSizes";
 import {EmptyScreen} from "~/components/EmptyScreen";
 import {ImageResources} from "~/common/ImageResources.g";
+import {data} from "~/infrastructure/mocks/mockAdvertises";
+import {ListItem} from "~/modules/main/components/ListItem";
+import {CommonSizes} from "~/core/theme/commonSizes";
 
 export function MyAdvertisesList() {
-    const [data] = useState([
-        {
-            id: '1',
-            title: 'Продажа велосипеда',
-            body: 'Почти новый горный велосипед, бренд XYZ, 2023 года выпуска. Использовался всего несколько месяцев',
-            price: '1200 руб.',
-            location: 'Тирасполь',
-            date: 'сегодня в 13:04',
-            image: null
-        }
-    ]);
-
     return (
-        <>
-            {data ? <FlatList
-                data={data}
-                keyExtractor={item => item.id}
-                renderItem={({item}) => {
-                    return <ListItem item={item}/>;
-                }}
-                showsVerticalScrollIndicator={false}
-                contentContainerStyle={styles.content}
-            /> : <EmptyScreen
-                image={ImageResources.plus}
-                title={"emptyScreen.myAdvertises.title"}
-                text={"emptyScreen.myAdvertises.text"}/>}
-        </>
+        <FlatList
+            data={[data[0]]}
+            renderItem={({item}) => {
+                return <ListItem item={item}/>;
+            }}
+            ListEmptyComponent={() =>
+                <EmptyScreen
+                    image={ImageResources.plus}
+                    title={"emptyScreen.myAdvertises.title"}
+                    text={"emptyScreen.myAdvertises.text"}
+                />
+            }
+            contentContainerStyle={styles.content}
+        />
     );
 }
 
