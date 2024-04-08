2a31982a76c792af2580e36345e8f3ce98566ad6
Alexey <kaluzhniy2017@yandex.ru>
2024-04-08 10:52:34 +0300

    added change icon when press on favorite button

--- src/components/FavoriteButton.tsx
+++ src/components/FavoriteButton.tsx
@@ -1,10 +1,20 @@
 import {TouchableOpacity} from "react-native";
 import HeartInactive from "../../resources/icons/heartInactive.svg";
+import HeartActive from "../../resources/icons/heartActive.svg";
+import {useState} from "react";
+import {useThemeColors} from "~/core/theme/hooks";
 
 export function FavoriteButton() {
+    const [isFavorite, setFavorite] = useState(false);
+    const colors = useThemeColors();
+
+    const handleSetFavorite = () => {
+        setFavorite(prev => !prev);
+    };
+
     return (
-        <TouchableOpacity>
-            <HeartInactive/>
+        <TouchableOpacity onPress={handleSetFavorite}>
+            {isFavorite ? <HeartActive color={colors.main}/> : <HeartInactive color={colors.outline}/>}
         </TouchableOpacity>
     );
 }
